30b10165efc70296ec2381b10ffc39734e4db63b
Richard Tattersall <lordtatty@googlemail.com>
2018-03-21 17:27:47 +0000

    build script should return non-zero exit status if a command returns the same

--- start-build.sh
+++ start-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 sh ./scripts/install-deps.sh
 docker-compose -f ./docker-compose-yml/docker-compose.yml -p dockerComposeTestNetwork up -d --build
 sh ./scripts/run-browser-tests.sh
