2229d0f47c7339d334fc7213f70e912ecf1a10d3
Richard Tattersall <lordtatty@googlemail.com>
2018-05-09 01:19:22 +0100

    shut down firefox container when tests completed

--- build/build-scripts/run-browser-tests.sh
+++ build/build-scripts/run-browser-tests.sh
@@ -2,3 +2,4 @@
 set -e
 docker-compose -f ./build/docker-compose-yml/docker-compose.start-firefox.yml up -d --build
 docker-compose -f ./build/docker-compose-yml/docker-compose.run-tests.yml run --rm codecept
+docker-compose -f ./build/docker-compose-yml/docker-compose.start-firefox.yml down
