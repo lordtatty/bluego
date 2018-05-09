c55f5964f70321cb45c7b18a2e0a4bb2f52ca4d0
Richard Tattersall <lordtatty@googlemail.com>
2018-05-09 20:18:17 +0100

    Fix docker network naming inconsistencies

--- build/build-scripts/start-build.sh
+++ build/build-scripts/start-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 sh ./build/build-scripts/install-deps.sh
-docker-compose -f ./build/docker-compose-yml/docker-compose.yml -p dockerComposeTestNetwork up -d --build
+docker-compose -f ./build/docker-compose-yml/docker-compose.yml -p dockercomposetestnetwork up -d --build
 sh ./build/build-scripts/run-unit-tests.sh
 sh ./build/build-scripts/run-browser-tests.sh
--- start-dev.sh
+++ start-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 sh ./build/build-scripts/install-deps.sh
-docker-compose -f ./build/docker-compose-yml/docker-compose.yml -f ./build/docker-compose-yml/docker-compose-dev-overrides.yml -p docker-compose-test-network up --build
+docker-compose -f ./build/docker-compose-yml/docker-compose.yml -f ./build/docker-compose-yml/docker-compose-dev-overrides.yml -p dockercomposetestnetwork up --build
