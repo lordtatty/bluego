59dd6fdd69dd9f040e068f45c3eccdff2660941c
Richard Tattersall <lordtatty@googlemail.com>
2018-07-02 00:01:25 +0100

    Separate integration tests from browser tests

--- build/build-scripts/run-all-tests.sh
+++ build/build-scripts/run-all-tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 sh ./build/build-scripts/run-unit-tests.sh
+sh ./build/build-scripts/run-integration-tests.sh
 sh ./build/build-scripts/run-browser-tests.sh
--- build/build-scripts/run-browser-tests.sh
+++ build/build-scripts/run-browser-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 docker-compose -f ./build/docker-compose-yml/docker-compose.start-firefox.yml up -d --build
-docker-compose -f ./build/docker-compose-yml/docker-compose.run-tests.yml run --rm codecept
+docker-compose -f ./build/docker-compose-yml/docker-compose.run-tests.yml run --rm codecept-browser-tests
 docker-compose -f ./build/docker-compose-yml/docker-compose.start-firefox.yml down
