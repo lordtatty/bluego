8fa4cc70cfbcff30d78a3b3a1f1a3b0c66238698
Richard Tattersall <lordtatty@googlemail.com>
2018-07-07 23:06:47 +0100

    Needs to be prettier, but will do for now

--- build/build-scripts/start-build.sh
+++ build/build-scripts/start-build.sh
@@ -2,5 +2,5 @@
 set -e
 sh ./build/build-scripts/install-deps.sh
 docker-compose -f ./build/docker-compose-yml/docker-compose.yml -p dockercomposetestnetwork up -d --build
-sh ./build/build-scripts/run-unit-tests.sh
-sh ./build/build-scripts/run-browser-tests.sh
+sleep 10 # Dirty hack to ensure mongodb is up and running before starting the tests.  I wonder if I can poll it?
+sh ./build/build-scripts/run-all-tests.sh
